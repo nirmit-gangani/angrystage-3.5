d7b8f23de880be06d766ed11b13084a86667c740
nirmit-gangani <nirmitgangani@gmail.com>
2021-06-08 03:00:18 -0700

    test

--- sketch.js
+++ sketch.js
@@ -37,8 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    Slingshot= new Slingshot(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot= new Slingshot(bird.body,{x:200,y:100});
 }
 
 function draw(){
@@ -62,6 +62,14 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    Slingshot.display();    
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX , y:mouseY})
+}
+
+function mouseReleased(){
+    slingshot.fly();
 }
